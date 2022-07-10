72d3a3ce1568927555c458338d51cd77c6c3de45
DipPal513m <dip.pal.513@gmail.com>
2022-07-10 19:53:41 +0600

    s

---
+++ index.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+btn.addEventListener('click',function(){
+    h1.innerHTML = 'তুই একডা কুত্তা বদমাইশ ফাজিল'
+})
